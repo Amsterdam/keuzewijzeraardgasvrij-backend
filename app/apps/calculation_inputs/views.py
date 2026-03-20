e748eed78de7693ed97333dafd01901bc4c8ef95
Nino <n.van.der.laan@amsterdam.nl>
2026-03-20 07:52:59 +0100

    add debug logs

--- app/apps/calculation_inputs/views.py
+++ app/apps/calculation_inputs/views.py
@@ -1,14 +1,37 @@
-from rest_framework import generics, throttling
+from rest_framework import generics
 
 from .models import CalculationInput
 from .serializers import CalculationInputCreateSerializer
 
 
-class CalculationInputThrottle(throttling.AnonRateThrottle):
+from rest_framework.throttling import AnonRateThrottle
+
+
+class DebugAnonThrottle(AnonRateThrottle):
     scope = "calculation_input"
+
+    def get_ident(self, request):
+        meta = request.META
+
+        xff = meta.get("HTTP_X_FORWARDED_FOR")
+        real_ip = meta.get("HTTP_X_REAL_IP")
+        remote_addr = meta.get("REMOTE_ADDR")
+
+        ident = super().get_ident(request)
+
+        print(
+            {
+                "X_FORWARDED_FOR": xff,
+                "X_REAL_IP": real_ip,
+                "REMOTE_ADDR": remote_addr,
+                "USED_IDENT": ident,
+            }
+        )
+
+        return ident
 
 
 class CalculationInputCreateView(generics.CreateAPIView):
     queryset = CalculationInput.objects.all()
     serializer_class = CalculationInputCreateSerializer
-    throttle_classes = [CalculationInputThrottle]
+    throttle_classes = [DebugAnonThrottle]
